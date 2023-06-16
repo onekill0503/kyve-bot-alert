54774a27df0dfb000ec1d91c0d19577330206a83
onekill0503 <gamemasteraji123@gmail.com>
2023-06-16 12:50:18 +0700

    [FIX]: notification function error

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-import bot from "./src/bot.js"
+import telegram from "./src/bot.js"
 import { config } from "dotenv";
+import fetch from "./src/fetch.js";
 
 config();
+// telegram user Id to send notification
 const NotifTo = process.env.USER_NOTIF;
-const time = 3600000;
+// timer 1 hour at interval
+const time = 5000;
 
 if(NotifTo){
     // create notification
@@ -11,12 +14,12 @@ if(NotifTo){
         const kyveBlock = await fetch.fetchKyveBlock();
         const localBlock = await fetch.fetchLocalRpcBlock();
         if(localBlock < 0 || kyveBlock < 0) {
-            ctx.reply(`❌ Failed get current block`)
+            telegram.client.sendMessage(NotifTo , `❌ Failed get current block`);
             return;
         };
         console.log(`Kyve Block : ${kyveBlock}\tLocal Block : ${localBlock}`)
-        ctx.reply(`✅ Current GAP from local node with kyve node is ${kyveBlock - localBlock} Blocks`);
+        telegram.client.sendMessage(NotifTo , `✅ Current GAP from local node with kyve node is ${kyveBlock - localBlock} Blocks`)
     } , time);
 }
 
-bot.bot.launch();
+telegram.bot.launch();
